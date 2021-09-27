0daf08f694d703f29b35119b6e3d7336acb3324c
Christoffer Mattsson Langseth <christoffer.langseth@scilifelab.se>
2021-09-27 09:45:34 +0200

    Add files via upload

---
+++ ISS_processing/__init__.py
@@ -0,0 +1,6 @@
+from ISS_processing.preprocessing import (zen_OME_tiff, 
+                                            ashlar_wrapper, 
+                                            reshape_split, 
+                                            leica_mipping,
+                                            leica_OME_tiff,
+                                            tile_stitched_images)
---
+++ README.md
@@ -0,0 +1,1 @@
+ISS_processing is a Python package used to process In situ sequencinig data. This includes the preprocessing of the data, including alignment and stitching (using [ASHLAR](https://github.com/labsyspharm/ashlar)) and retiling. In addition, there is also a starfish decoding module for formatting of aligned and retiled data. 
