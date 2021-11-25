7c64e364e1c7e97dd81c6db4f0c608e9498e2170
Christoffer Mattsson Langseth <christoffer.langseth@scilifelab.se>
2021-11-25 09:43:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from ISS_processing.preprocessing import (zen_OME_tiff, 
-                                            ashlar_wrapper, 
-                                            reshape_split, 
-                                            leica_mipping,
-                                            leica_OME_tiff,
-                                            tile_stitched_images, 
-                                         preprocessing_main_leica)
+                                          ashlar_wrapper, 
+                                          reshape_split, 
+                                          leica_mipping,
+                                          leica_OME_tiff,
+                                          tile_stitched_images, 
+                                          preprocessing_main_leica)
